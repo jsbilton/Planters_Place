f7e8cbcdb09c4c69bc3f0ce6a70a40844027d434
Jared Bilton <jsbilton@gmail.com>
2017-01-05 16:11:34 -0500

    step1 - app - home - about

---
+++ web/src/pages/about.js
@@ -0,0 +1,12 @@
+const React = require('react')
+
+const About = React.createClass({
+  render () {
+    return (
+      <h1>
+        About
+      </h1>
+    )
+  }
+})
+module.exports = About
---
+++ web/src/pages/home.js
@@ -0,0 +1,13 @@
+const React = require('react')
+
+const Home = React.createClass({
+  render () {
+    return (
+      <h1>
+        Welcome Home
+      </h1>
+    )
+  }
+})
+
+module.exports = Home

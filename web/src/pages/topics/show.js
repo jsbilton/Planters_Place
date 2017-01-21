d1dbcc658f8666eba24cf63d28026e4e0b99fd54
Jared Bilton <jsbilton@gmail.com>
2017-01-20 23:05:00 -0500

    topic remove

--- web/src/pages/topics/show.js
+++ web/src/pages/topics/show.js
@@ -1,11 +1,12 @@
 const React = require('react')
 const xhr = require('xhr')
-const {Link} = require('react-router')
+const {Link, Redirect} = require('react-router')
 
 const Topic = React.createClass({
   getInitialState() {
     return {
-      topic: {}
+      topic: {},
+      removed: false
     }
   },
   componentDidMount() {
@@ -14,11 +15,28 @@ const Topic = React.createClass({
       this.setState({topic})
     })
   },
+  handleRemove(e) {
+    e.preventDefault()
+    if (confirm("Sure?")) {
+      xhr.del('http://localhost:4000/topics/' + this.state.topic.id, { json: this.state.topic}, (e, r, body) => {
+        if (e) return console.log(e.message)
+        this.setState({removed: true})
+      })
+    }
+  },
   render() {
     return (
       <div>
+        {this.state.removed ? <Redirect to="/topics" /> : null}
         <h3>{this.state.topic.name}</h3>
+        <p>{this.state.topic.description}</p>
+        <p>{this.state.topic.category}</p>
+        <p>{this.state.topic.location}</p>
+        <p>{this.state.topic.reported_by}</p>
         <Link to={`/topics/${this.state.topic.id}/edit`}>Edit</Link>
+        |
+        <a href="#" onClick={this.handleRemove}>Remove</a>
+        |
         <Link to="/topics">Return</Link>
       </div>
     )
